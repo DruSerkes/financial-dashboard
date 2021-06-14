c5a64310b7fde72280588afd9da41b20acc6b992
Dru Serkes <dru@zephyrx.com>
2021-06-14 18:46:40 -0400

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
 it('should render without breaking', () => {
   render(<App />);
