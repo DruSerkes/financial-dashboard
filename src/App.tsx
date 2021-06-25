2e8840c50538e5e1b352708872f81661a4a1c2a6
Dru Serkes <dru@zephyrx.com>
2021-06-25 14:53:17 -0400

    add TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,16 @@ import './App.css';
 // tutorial: https://www.telerik.com/blogs/lets-build-a-financial-dashboard-with-react?utm_medium=cpm&utm_source=jsweekly&utm_campaign=kendo-ui-react-blog-fin-dashboard&utm_content=sponsored-link#step-2-adding-ui-components
 // github: https://github.com/tjvantoll/financial-dashboard/tree/start
 
+// TODO 
+/* 
+0. Create dummy data 
+1. Create Info panel
+2. Create Allocation pie-chart 
+3. Create Performance line-chart 
+4. Create Positions table 
+5. Style it 
+6. Did you write tests?? 
+*/
 export const App: React.FC = () => {
   const classes = useStyles();
 
--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -8,16 +8,20 @@ export const Home: React.FC = () => {
     <Box className="Home">
       <Box className="Home-Info">
         <p>Info</p>
+        {/* Dropdown for team / info */}
       </Box>
 
       <Box className="Home-Allocation">
         <p>Allocation</p>
+        {/* TODO Pie chart for allocation data */}
       </Box>
       <Box className="Home-Performance">
         <p>Performance</p>
+        {/* TODO Line chart for performance data */}
       </Box>
       <Box className="Home-Positions">
         <p>Positions</p>
+        {/* TODO table for positions info */}
       </Box>
 
     </Box>
